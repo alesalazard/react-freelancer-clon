1cfb8454846d0993b7de49b2b33ef2d84966e458
alexandretta <alexsalazar21@gmail.com>
2023-02-13 16:28:03 -0500

    se diseña el primer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Section1 from './components/Section1';
 // import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section1 />
       {/* <Footer /> */}
     </div>
   );
---
+++ src/components/Section1.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "bootstrap";
+import "../styles/Section1.css";
+
+const Section1 = () => {
+  return (
+    <div className='container-fluid container1'>
+      <div className='row'>
+        <div className='col'>
+          <div className='profile'>
+            <img
+              src={require("../img/profile.png")}
+              alt='perfil'
+              className='img-profile'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='title1'>START BOOTSTRAP</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-2 divider'></div>
+        <div className="col-2 material-symbols-rounded">star</div>
+        <div className='col-2 divider'></div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <p className='experience'>
+            Web developer - Graphic Artist - User Experience Designer
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Section1;
